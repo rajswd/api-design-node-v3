13c41eb920a8b90189a0059e5f6a7fe4135a89a6
Raj Singh <rsingh@venteclife.com>
2021-06-27 22:16:13 +0800

    Express: Routes and logs work.

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,21 @@
 import { Router } from 'express'
 
+const controller = (req, res, next) => {
+  res.send({ message: 'Hello items...' })
+}
+
 const router = Router()
+// api/item
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+// /api/item
+router
+  .route('/:id')
+  .put(controller)
+  .delete(controller)
+  .get(controller)
 
 export default router
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import morgan from 'morgan'
 import cors from 'cors'
+import router from './resources/item/item.router'
 
 export const app = express()
 
@@ -11,5 +12,56 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+/**
+ * Adding middleware in multiple ways
+ */
+const executeBeforeEveryCall = (req, res, next) => {
+  console.log('---------executeBeforeEveryCall---------')
+  // WE can also pass data to next handler ex
+  req.addOnData = {
+    middlewareRequest: ' --- executeBeforeEveryCall middleware Data ----'
+  }
+  res.addOnData = { middlewareResponse: ' --- executeBeforeEveryCall ----' }
+  next()
+}
 
-export const start = () => {}
+const log = (req, res, next) => {
+  console.log('----LOG--1----')
+  next()
+}
+
+const log2 = (req, res, next) => {
+  console.log('----LOG--2----')
+  next()
+}
+// Adding middle ware which will execute before every api call.
+app.use(executeBeforeEveryCall)
+
+app.get('/', [log, log2], (req, res) => {
+  res.send(
+    Object.assign(
+      {},
+      {
+        msg: '------HELLO EXPRESS-----',
+        middlewareDataReq: req.addOnData,
+        middlewareDataRes: res.addOnData
+      }
+    )
+  )
+})
+
+app.post('/', log2, (req, res) => {
+  console.log('  payload  : ', req.body)
+  res.send({ msg: 'OK', middlewareData: res.addOnData })
+})
+
+/**
+ * Mounting Item module Sub router here
+ */
+app.use('/api/item', router)
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('--Listen at 30000---')
+  })
+}
